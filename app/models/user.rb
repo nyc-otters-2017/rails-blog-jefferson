af0bdb25776d57087272ff1a23e341ffad1ecba2
Jefferson-Faseler <jjfaseler@gmail.com>
2017-01-21 16:42:55 -0500

    Add custom email validation to user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
-  validates :name, presence: true
+  validates :name, :email, :password, presence: true
   validates :bio, length: {maximum: 500}
   validates :location, length: {maximum: 20}
+  validates :password, length: {in: 8..20}
+
+  validate :valid_email
+
+  def valid_email
+    if !email.match(/\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/)
+      errors.add(:email, 'Email is not valid')
+    end
+  end
+
 end
