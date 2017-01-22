e2b07a0089ab57485eca668666e3162b2b3b74b4
Jefferson-Faseler <jjfaseler@gmail.com>
2017-01-22 08:45:44 -0500

    remove current user helper method from users controller and make new helper method file for sessions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,5 @@
 class UsersController < ApplicationController
 
-  def current_user
-    return unless session[:user_id]
-    @current_user ||= User.find(session[:user_id])
-  end
-
   def index
     @users = User.all
   end
