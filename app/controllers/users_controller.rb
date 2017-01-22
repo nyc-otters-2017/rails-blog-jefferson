8507273b46242fc8ba42b191ca8170e4b030fc76
Jefferson-Faseler <jjfaseler@gmail.com>
2017-01-21 21:05:08 -0500

    Add user controller actions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,56 @@
 class UsersController < ApplicationController
 
+  # def current_user
+  #   return unless session[:user_id]
+  #   @current_user ||= User.find(session[:user_id])
+  # end
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
-  
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to user_path
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :bio, :location, :email, :password_digest)
+    end
+
 end
