16e7d79a3d8ca6711feda2ec7b7c58569f695198
Jefferson-Faseler <jjfaseler@gmail.com>
2017-01-22 08:24:26 -0500

    Add sessions controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # get '/users/index' => 'users#index'
-  # get '/users/new' => 'users#new'
-  # post '/users' => 'users#create'
-  # get '/users/:id' => 'users#show'
-  # get '/users/:id/edit' => 'users#edit'
-  # patch '/users/:id' => 'users#update'
-  # delete '/users/:id' => 'users#destroy'
-
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
+  
   resources :users
 
 end
