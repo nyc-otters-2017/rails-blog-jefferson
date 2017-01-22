6a2b203c23daf03586c9c7de9cbc5e1ba867546b
Jefferson-Faseler <jjfaseler@gmail.com>
2017-01-21 19:11:14 -0500

    Add proper routing and update view page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/users' => 'users#index'
-  
+  get '/users/index' => 'users#index'
+  get '/users/new' => 'users#new'
+  post '/users' => 'users#create'
+  get '/users/:id' => 'users#show'
+  get '/users/:id/edit' => 'users#edit'
+  patch '/users/:id' => 'users#update'
+  delete '/users/:id' => 'users#destroy'
+
 end
