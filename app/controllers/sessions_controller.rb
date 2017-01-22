2cb0546d32530d07ac5a08a77d0f3753d94451c6
Jefferson-Faseler <jjfaseler@gmail.com>
2017-01-22 08:40:31 -0500

    Add session creation on successful login in sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: parmas[:sessions][:email])
+    if user && user.authenticate(params[:sessions][:password])
+      session[:user_id] = user.id
+      redirect_to user
+    else
+      render 'new'
+    end
   end
 
   def destroy
